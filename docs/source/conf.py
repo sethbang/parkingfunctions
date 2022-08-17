2911f0b89144a77a26eadca7ea1e79516fd93dcd
Seth Bangert <seth@sbang.me>
2022-08-16 23:32:20 -0600

    Updated conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ master_doc = 'index'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    "myst_parser",
+    "myst-parser",
     "sphinx.ext.duration",
     "sphinx.ext.autosectionlabel",
     "sphinx.ext.napoleon",
